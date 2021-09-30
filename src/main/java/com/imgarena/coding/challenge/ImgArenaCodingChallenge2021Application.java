2de02e488e8c261f6c07948424dd4a75fe78a1bd
Elliot Ball <elliotjordball@gmail.com>
2021-09-30 19:52:37 +0100

    Initial commit

---
+++ src/main/java/com/imgarena/coding/challenge/ImgArenaCodingChallenge2021Application.java
@@ -0,0 +1,13 @@
+package com.imgarena.coding.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImgArenaCodingChallenge2021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImgArenaCodingChallenge2021Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/imgarena/coding/challenge/ImgArenaCodingChallenge2021ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imgarena.coding.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImgArenaCodingChallenge2021ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
